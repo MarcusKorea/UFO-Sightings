750e9a9a6a3ead1806279a02a8d97280af8bd8d5
MarcusKorea <marcus.a.correia@gmail.com>
2021-11-10 19:16:47 +1100

    comepleted first level

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -7,7 +7,7 @@ var tbody = d3.select("tbody");
 // create and add a table
 tableData.forEach(function(object){
     // puts it to the log
-    // console.log(object);
+    console.log(object);
 
     // creates a row in the table
     var row = tbody.append("tr");
@@ -32,7 +32,7 @@ tableData.forEach(function(object){
 var button = d3.select("#filter-btn")
 
 // select the form
-var form = d3.select("#form-group");
+var form = d3.select("#form");
 
 // create handlers
 form.on("submit",runEnter);
@@ -41,6 +41,9 @@ button.on("click",runEnter);
 function runEnter(){
     // Prevent the page from refreshing
     d3.event.preventDefault();
+
+    // This clears the table data 
+    d3.select("tbody").selectAll("tr").remove()
   
     // Select the input element and get the raw HTML node
     var inputElement = d3.select("#datetime");
@@ -50,10 +53,29 @@ function runEnter(){
 
     var filteredData = tableData.filter(row => row.datetime === inputValue);
 
-
-    // console.log(inputValue);
     console.log(filteredData);
+
+    // add the filtered data table
+    filteredData.forEach(function(object){
+    // creates a row in the table
+    var row = tbody.append("tr");
+
+    // loop through object and grab ket value pairs
+    Object.entries(object).forEach(function([key,value]){
+
+        // print to the log
+        // console.log(key,value);
+
+        // append to the row
+        var cell = row.append("td");
+
+        // add text to the cell
+        cell.text(value);
+    });
+});
 }
+
+
 
 
 
---
+++ UFO-level-2/static/js/app.js
@@ -0,0 +1,81 @@
+// from data.js
+var tableData = data;
+
+//select tbody
+var tbody = d3.select("tbody");
+
+// create and add a table
+tableData.forEach(function(object){
+    // puts it to the log
+    console.log(object);
+
+    // creates a row in the table
+    var row = tbody.append("tr");
+
+    // loop through object and grab ket value pairs
+    Object.entries(object).forEach(function([key,value]){
+
+        // print to the log
+        // console.log(key,value);
+
+        // append to the row
+        var cell = row.append("td");
+
+        // add text to the cell
+        cell.text(value);
+    });
+});
+
+// get text from the filter
+
+// select the button
+var button = d3.select("#filter-btn")
+
+// select the form
+var form = d3.select("#form");
+
+// create handlers
+form.on("submit",runEnter);
+button.on("click",runEnter);
+
+function runEnter(){
+    // Prevent the page from refreshing
+    d3.event.preventDefault();
+
+    // This clears the table data 
+    d3.select("tbody").selectAll("tr").remove()
+  
+    // Select the input element and get the raw HTML node
+    var inputElement = d3.select("#datetime");
+  
+    // Get the value property of the input element
+    var inputValue = inputElement.property("value");
+
+    var filteredData = tableData.filter(row => row.datetime === inputValue);
+
+    console.log(filteredData);
+
+    // add the filtered data table
+    filteredData.forEach(function(object){
+    // creates a row in the table
+    var row = tbody.append("tr");
+
+    // loop through object and grab ket value pairs
+    Object.entries(object).forEach(function([key,value]){
+
+        // print to the log
+        // console.log(key,value);
+
+        // append to the row
+        var cell = row.append("td");
+
+        // add text to the cell
+        cell.text(value);
+    });
+});
+}
+
+
+
+
+
